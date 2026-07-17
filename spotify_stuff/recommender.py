694425adcc7c4208c55c7c4dab03b55d9f3a625b
average-us <roger.z.cui@icloud.com>
2026-07-16 22:06:47 -0500

    Improve recommender

--- spotify_stuff/recommender.py
+++ spotify_stuff/recommender.py
@@ -7,6 +7,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+import time
 
 load_dotenv()
 
@@ -14,12 +15,18 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 
+last_track_id = None
+selected_track_id = None
+
+listened_tracks = []
+listened_artists = []
+
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         redirect_uri=REDIRECT_URI,
-        scope="user-read-currently-playing user-read-playback-state"
+        scope="user-read-currently-playing user-read-playback-state user-modify-playback-state"
     )
 )
 
@@ -29,63 +36,67 @@ model.load_model("models/skip_model.cbm")
 feature_columns = joblib.load("models/features.pkl")
 playlist = pd.read_csv("data/playlist.csv")
 
-current_playback = sp.current_playback()
+while True:
+    current_playback = sp.current_playback()
 
-track = current_playback["item"]
+    track = current_playback["item"]
 
+    current_track_id = track["id"]
 
-current_context = {
-    "previous_track_id": track["id"],
-    "previous_artist_id": track["artists"][0]["id"],
-    "hour": datetime.now().hour,
-    "weekday": datetime.now().weekday()
-}
+    if (current_track_id != last_track_id) and (last_track_id is not None):
 
-hour_sin = np.sin(2 * np.pi * current_context["hour"] / 24)
-hour_cos = np.cos(2 * np.pi * current_context["hour"] / 24)
+        listened_tracks.append(last_track_id)
+        listened_artists.append(sp.track(last_track_id)["artists"][0]["id"])
 
-weekday_sin = np.sin(2 * np.pi * current_context["weekday"] / 7)
-weekday_cos = np.cos(2 * np.pi * current_context["weekday"] / 7)
+        current_context = {
+            "previous_track_id": last_track_id,
+            "previous_artist_id": sp.track(last_track_id)["artists"][0]["id"],
+            "hour": datetime.now().hour,
+            "weekday": datetime.now().weekday()
+        }
 
-rows = []
+        hour_sin = np.sin(2 * np.pi * current_context["hour"] / 24)
+        hour_cos = np.cos(2 * np.pi * current_context["hour"] / 24)
 
-for _, song in playlist.iterrows():
-    rows.append({
-        "track_id": song["track_id"],
-        "artist_id": song["artist_id"],
-        "previous_track_id": current_context["previous_track_id"],
-        "previous_artist_id": current_context["previous_artist_id"],
-        "explicit": song["explicit"],
-        "duration": song["duration"],
-        "same_artist": int(song["artist_id"] == current_context["previous_artist_id"]),
-        "same_track": 0,  # never recommend current song again
-        "hour_sin": hour_sin,
-        "hour_cos": hour_cos,
-        "weekday_sin": weekday_sin,
-        "weekday_cos": weekday_sin
-    })
+        weekday_sin = np.sin(2 * np.pi * current_context["weekday"] / 7)
+        weekday_cos = np.cos(2 * np.pi * current_context["weekday"] / 7)
 
-candidate_df = pd.DataFrame(rows)
+        rows = []
 
-candidate_df = candidate_df[feature_columns]
+        for _, song in playlist.iterrows():
+            rows.append({
+                "track_id": song["track_id"],
+                "artist_id": song["artist_id"],
+                "previous_track_id": current_context["previous_track_id"],
+                "previous_artist_id": current_context["previous_artist_id"],
+                "explicit": song["explicit"],
+                "duration": song["duration"],
+                "hour_sin": hour_sin,
+                "hour_cos": hour_cos,
+                "weekday_sin": weekday_sin,
+                "weekday_cos": weekday_cos
+            })
 
-skip_probs = model.predict_proba(candidate_df)[:, 1]
+        candidate_df = pd.DataFrame(rows)
 
-candidate_df["skip_probability"] = skip_probs
-candidate_df = candidate_df.sort_values("skip_probability")
+        candidate_df = candidate_df[feature_columns]
 
-print("\nTop 10 recommended songs:\n")
+        skip_probs = model.predict_proba(candidate_df)[:, 1]
 
-top10 = candidate_df.head(10)
+        candidate_df["skip_probability"] = skip_probs
+        candidate_df = candidate_df.sort_values("skip_probability")
 
-for i, row in top10.iterrows():
-    song = playlist.iloc[i]
-    print(
-        f"{sp.track(song['track_id'])['name']} | "
-        f"skip_prob={row['skip_probability']:.3f}"
-    )
+        for i, row in candidate_df.iterrows():
+            song = playlist.iloc[i]
 
-best = playlist.iloc[candidate_df.index[0]]
+            if song["track_id"] not in listened_tracks and (song["artist_id"] not in listened_artists[-5:]):
+                selected_track_id = song["track_id"]
+                break
 
-print("\nBEST RECOMMENDATION:")
-print(sp.track(best["track_id"])["name"])
+            continue
+
+        sp.add_to_queue(uri=f"spotify:track:{selected_track_id}")
+    
+    last_track_id = current_track_id
+
+    time.sleep(5)
